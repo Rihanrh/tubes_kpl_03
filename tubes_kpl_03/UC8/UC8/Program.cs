ed7e9f87de84194459bb5f009d65cf6f8f578cda
raliauliya <121374492+raliauliya@users.noreply.github.com>
2023-05-14 19:48:54 +0700

    First Commit

---
+++ tubes_kpl_03/UC8/UC8/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public class Order
+{
+    public int OrderId { get; set; }
+    public string CustomerName { get; set; }
+    public OrderStatus Status { get; set; }
+}
+
+public enum OrderStatus
+{
+    ConfirmationPayment,
+    InProgress,
+    Completed,
+    OrderDone
+}
+
+public class OrderManager
+{
+    private static readonly Order[] Orders =
+    {
+        new Order { OrderId = 1, CustomerName = "Putri Auliya", Status = OrderStatus.ConfirmationPayment },
+        new Order { OrderId = 2, CustomerName = "Auliya", Status = OrderStatus.InProgress },
+        new Order { OrderId = 3, CustomerName = "Rahmah", Status = OrderStatus.OrderDone },
+        new Order { OrderId = 4, CustomerName = "Auliya Rahmah", Status = OrderStatus.Completed },
+        new Order { OrderId = 5, CustomerName = "Putri Rahmah", Status = OrderStatus.ConfirmationPayment}
+    };
+
+    public void DisplayOrders()
+    {
+        Console.WriteLine("Current Orders:");
+
+        int queueNumber = 1;
+
+        foreach (Order order in Orders)
+        {
+            if (order.Status != OrderStatus.OrderDone)
+            {
+                Console.WriteLine($"#{queueNumber} - Order ID: {order.OrderId}, Customer Name: {order.CustomerName}, Status: {order.Status}");
+                queueNumber++;
+            }
+        }
+
+        Console.WriteLine();
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        OrderManager manager = new OrderManager();
+        manager.DisplayOrders();
+    }
+}
