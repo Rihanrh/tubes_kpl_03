fe0f99bd72438c26f067f954e452bf662df31acc
hifahir <putratertuahirzi@gmail.com>
2023-05-14 23:49:48 +0700

    FIx implemet and FIx DbC

--- tubes_kpl_03/CheckoutMenuMakanan/Program.cs
+++ tubes_kpl_03/CheckoutMenuMakanan/Program.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 
-class Program
+namespace CheckoutMenuMakanan
 {
-    static void Main(string[] args)
+    public class Program
     {
-        Dictionary<string, int> makanan = new Dictionary<string, int>()
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> makanan = new Dictionary<string, int>()
         {
             {"Nasi Goreng", 15000 },
             {"Katsu", 18000 },
@@ -15,58 +17,61 @@ class Program
             {"N/A", 0 }
         };
 
-        Dictionary<string, int> minuman = new Dictionary<string, int>() {
+            Dictionary<string, int> minuman = new Dictionary<string, int>() {
             {"Air Mineral", 4000 },
             {"Es Teh Manis", 5000 },
             {"Lemon Tea", 5000 },
             {"N/A", 0 }
         };
 
-        //Menampilkan daftar makanan
-        Console.WriteLine("Makanan");
-        int i = 1;
-        foreach (KeyValuePair<String, int> entry in makanan)
-        {
-            Console.WriteLine($"{i}. {entry.Key} (Rp {entry.Value})");
-            i++;
+            //Menampilkan daftar makanan
+            Console.WriteLine("Makanan");
+            int i = 1;
+            foreach (KeyValuePair<string, int> entry in makanan)
+            {
+                Console.WriteLine($"{i}. {entry.Key} (Rp {entry.Value})");
+                i++;
+            }
+
+            Console.WriteLine(" ");
+
+            //Menampilkan daftar minuman
+            Console.WriteLine("Minuman");
+            i = 1;
+            foreach (KeyValuePair<string, int> entry in minuman)
+            {
+                Console.WriteLine($"{i}.{entry.Key} (Rp {entry.Value})");
+                i++;
+            }
+
+            //inputan makanan dan minuman
+            Console.Write("Masukkan nomor pesanan makanan yang akan dimasukkan (pisahkan dengan koma): ");
+            string inputMakanan = Console.ReadLine();
+
+            Console.Write("Masukkan nomor pesanan minuman yang akan dimasukkan (pisahkan dengan koma): ");
+            string inputMinuman = Console.ReadLine();
+
+            //Memproses input pengguna
+            int total = hitungHargaTotal(makanan, inputMakanan) + hitungHargaTotal(minuman, inputMinuman);
+
+            // Menampilkan harga
+            Console.WriteLine($"Total harga: Rp{total}");
+            Console.ReadLine();
+
         }
 
-        Console.WriteLine(" ");
-
-        //Menampilkan daftar minuman
-        Console.WriteLine("Minuman");
-        i = 1;
-        foreach (KeyValuePair<string, int> entry in minuman)
+        public static int hitungHargaTotal(Dictionary<string, int> menu, string input)
         {
-            Console.WriteLine($"{i}.{entry.Key} (Rp {entry.Value})");
-            i++;
+            int total = 0;
+            string[] pilihan = input.Split(',');
+            foreach (string s in pilihan)
+            {
+                int index = int.Parse(s.Trim()) - 1;
+                total += menu.Values.ElementAt(index);
+            }
+            return total;
         }
 
-        //inputan makanan dan minuman
-        Console.Write("Masukkan nomor pesanan makanan yang akan dimasukkan (pisahkan dengan koma): ");
-        string inputMakanan = Console.ReadLine();
 
-        Console.Write("Masukkan nomor pesanan minuman yang akan dimasukkan (pisahkan dengan koma): ");
-        string inputMinuman = Console.ReadLine();
-
-        //Memproses input pengguna
-        int total = hitungHargaTotal(makanan, inputMakanan) + hitungHargaTotal(minuman, inputMinuman);
-
-        // Menampilkan harga
-        Console.WriteLine($"Total harga: Rp{total}");
-        Console.ReadLine();
-
-    }
-
-    static int hitungHargaTotal(Dictionary<string, int> menu, string input)
-    {
-        int total = 0;
-        string[] pilihan = input.Split(',');
-        foreach(string s in pilihan)
-        {
-            int index = int.Parse(s.Trim()) - 1;
-            total += menu.Values.ElementAt(index);
-        }
-        return total;
     }
 }
