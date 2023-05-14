40fb03ce9fb7e897ffaf9c43067a7757d1074af9
MrLI0N7 <93116969+MrLI0N7@users.noreply.github.com>
2023-05-14 16:20:29 +0800

    Implementasi Membuat Menu
    Dimana dapat membuat menu, menghapus, dan mengubah menu, dan juga ada untuk testing show all menu

---
+++ tubes_kpl_03/MainConsole/MenuMakanan.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainConsole
+{
+    public class MenuMakanan<T>
+    {
+        public string name_menu { get; set; }
+        public List<T> path_images { get; set; }
+        public double harga_menu { get; set; }
+        public string deskripsi_menu { get; set; }
+
+        public MenuMakanan(string name, List<T> listImages, double harga, string deskripsi)
+        {
+            Contract.Requires(!string.IsNullOrEmpty(name), "Name cannot be null or empty.");
+            Contract.Requires(listImages != null && listImages.Count > 0, "List of images cannot be null or empty.");
+            Contract.Requires(harga >= 0, "Price cannot be negative.");
+            Contract.Requires(!string.IsNullOrEmpty(deskripsi), "Description cannot be null or empty.");
+
+            this.name_menu = name;
+            this.path_images = listImages;
+            this.harga_menu = harga;
+            this.deskripsi_menu = deskripsi;
+        }
+
+        public void showImages()
+        {
+            Debug.Assert(this.path_images != null, "List of images cannot be null.");
+
+            for (int i = 0; i < path_images.Count; i++)
+            {
+                Console.WriteLine(path_images[i]);
+            }
+        }
+    }
+}
+
--- tubes_kpl_03/MainConsole/Program.cs
+++ tubes_kpl_03/MainConsole/Program.cs
@@ -1,7 +1,19 @@
-﻿class Program
+﻿using MainConsole;
+
+class Program
 {
     private static void Main(string[] args)
     {
+        Menu menu = new Menu();
         
+        menu.MenambahMenu(menu.MembuatMenu());
+        menu.MenambahMenu(menu.MembuatMenu());
+        menu.showAllMenu();
+
+        menu.UbahMenu("potato", "Big potato", 1000000, "its big", new List<string> { "g1", "g2" });
+        menu.showAllMenu();
+
+        menu.HapusMenu("Mie");
+        menu.showAllMenu();
     }
 }
