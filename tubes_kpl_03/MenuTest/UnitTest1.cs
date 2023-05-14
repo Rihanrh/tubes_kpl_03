3474153c562f50d99f1fd01703acc721b423f5b4
MrLI0N7 <93116969+MrLI0N7@users.noreply.github.com>
2023-05-14 23:36:42 +0800

    comments added

--- tubes_kpl_03/MenuTest/UnitTest1.cs
+++ tubes_kpl_03/MenuTest/UnitTest1.cs
@@ -9,17 +9,16 @@ namespace MenuTest
         [TestMethod]
         public void TestMembuatMenuValid()
         {
-            // Arrange
             MenuMakanan<string> expectedMenu = new MenuMakanan<string>("Nasi Goreng", new List<string> { "image1.png", "image2.png" }, 25000.0, "Nasi Goreng with chicken and vegetables.");
             Menu menu = new Menu();
 
             var stringReader = new StringReader("Nasi Goreng\nimage1.png\nY\nimage2.png\nN\n25000\nNasi Goreng with chicken and vegetables.");
             Console.SetIn(stringReader);
 
-            // Act
+
             MenuMakanan<string> actualMenu = menu.MembuatMenu();
 
-            // Assert
+            // seharusnya semua input yang sudah masuk itu sesuai pada objectnya
             Assert.AreEqual(expectedMenu.name_menu, actualMenu.name_menu);
             CollectionAssert.AreEqual(expectedMenu.path_images, actualMenu.path_images);
             Assert.AreEqual(expectedMenu.harga_menu, actualMenu.harga_menu);
@@ -28,46 +27,40 @@ namespace MenuTest
         [TestMethod]
         public void TestMenambahMenu()
         {
-            // Arrange
             var menu = new Menu();
             var menuMakanan = new MenuMakanan<string>("Nasi Goreng", new List<string> { "img1.jpg", "img2.jpg" }, 20000, "Nasi goreng enak");
 
-            // Act
             menu.MenambahMenu(menuMakanan);
 
-            // Assert
+            // Seharusnya list yang Daftar menu sudah ditambahkan satu menu
             Assert.AreEqual(1, menu.DaftarMenu.Count);
             Assert.AreEqual("Nasi Goreng", menu.DaftarMenu[0].name_menu);
         }
         [TestMethod]
         public void TestHapusMenu()
         {
-            // Arrange
             var menuMakanan1 = new MenuMakanan<string>("Nasi Goreng", new List<string> { "nasi_goreng.png" }, 15000, "Nasi goreng spesial");
             var menuMakanan2 = new MenuMakanan<string>("Mie Ayam", new List<string> { "mie_ayam.png" }, 12000, "Mie ayam pedas");
             var daftarMenu = new List<MenuMakanan<string>> { menuMakanan1, menuMakanan2 };
             var menu = new Menu { DaftarMenu = daftarMenu };
 
-            // Act
             menu.HapusMenu("Nasi Goreng");
 
-            // Assert
+            // Seharusnya pada list yang sudah ada item yang memiliki nama Nasi goreng terhapus
             Assert.AreEqual(1, daftarMenu.Count);
             Assert.IsFalse(daftarMenu.Any(menu => menu.name_menu.Equals("Nasi Goreng")));
         }
         [TestMethod]
         public void TestUbahMenu()
         {
-            // Arrange
             var menuMakanan = new MenuMakanan<string>("Nasi Goreng", new List<string> { "path/to/image1", "path/to/image2" }, 15000, "Nasi goreng dengan bumbu rempah yang lezat");
 
             var menu = new Menu();
             menu.MenambahMenu(menuMakanan);
 
-            // Act
             menu.UbahMenu("Nasi Goreng", "Nasi Goreng Spesial", 20000, "Nasi goreng dengan bumbu rempah yang lebih pedas", new List<string> { "path/to/image1", "path/to/image2", "path/to/image3" });
 
-            // Assert
+            // seharunya data dari Menu yang bernama Nasi Goreng diganti
             var updatedMenu = menu.DaftarMenu.FirstOrDefault(m => m.name_menu == "Nasi Goreng Spesial");
             Assert.IsNotNull(updatedMenu);
             Assert.AreEqual("Nasi Goreng Spesial", updatedMenu.name_menu);
